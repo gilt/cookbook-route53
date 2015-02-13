78872ceb64de5417034b89014b7d9cf84045b2b7
Graham Rhodes <grhodes@gilt.com>
2015-02-13 14:21:06 +0000

    Revert add recipe to config based

--- recipes/add.rb
+++ recipes/add.rb
@@ -3,7 +3,7 @@ include_recipe 'route53'
 
 route53_record "create a record" do
   name  node[:opsworks][:instance][:hostname] + "." + node[:r53][:zone_name]
-  value Net::HTTP.get(URI.parse("http://169.254.169.254/latest/meta-data/#{node[:r53][:ip_public]}"))
+  value Net::HTTP.get(URI.parse("http://169.254.169.254/latest/meta-data/#{node[:r53][:ip]}"))
   type  "A"
   ttl   node[:r53][:ttl].to_i
   zone_id               node[:r53][:zone_id]
