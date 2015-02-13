a0db08349ffe46173ae07b80b8dd2b2ce6ef5ef5
Graham Rhodes <grhodes@gilt.com>
2015-02-13 15:51:11 +0000

    Fix issue

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name        "r53"
 description "Update Route53 Zone"
 maintainer  "Gilt groupe"
 license     "Apache 2.0"
-version     "1.2.0"
+version     "1.3.0"
 
 depends "route53"
 
--- recipes/add_private_ip.rb
+++ recipes/add_private_ip.rb
@@ -3,7 +3,7 @@ include_recipe 'route53'
 
 route53_record "create a record" do
   name  node[:opsworks][:instance][:hostname] + "." + node[:r53][:zone_name]
-  value Net::HTTP.get(URI.parse("http://169.254.169.254/latest/meta-data/#{node[:r53][:ip_private]}"))
+  value Net::HTTP.get(URI.parse("http://169.254.169.254/latest/meta-data/local-ipv4"))
   type  "A"
   ttl   node[:r53][:ttl].to_i
   zone_id               node[:r53][:zone_id]
